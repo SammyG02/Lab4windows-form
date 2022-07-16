424ed6802294edab434e504b5d2847cb2d689656
Samuel Gosaye <samuelgosaye02@gmail.com>
2022-07-16 21:58:45 +0300

    Layout Created

--- lab4windowsform/lab4windowsform/Form1.Designer.cs
+++ lab4windowsform/lab4windowsform/Form1.Designer.cs
@@ -34,68 +34,158 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
+            this.numberBox = new System.Windows.Forms.TextBox();
+            this.dateBox = new System.Windows.Forms.TextBox();
+            this.inventoryNumBox = new System.Windows.Forms.TextBox();
+            this.objectNameBox = new System.Windows.Forms.TextBox();
+            this.countBox = new System.Windows.Forms.TextBox();
+            this.priceBox = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.cancelBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(56, 42);
+            this.label1.Location = new System.Drawing.Point(56, 36);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.Size = new System.Drawing.Size(65, 20);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Number";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(173, 42);
+            this.label2.Location = new System.Drawing.Point(258, 36);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.Size = new System.Drawing.Size(44, 20);
             this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
+            this.label2.Text = "Date";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(243, 190);
+            this.label3.Location = new System.Drawing.Point(479, 36);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(51, 20);
+            this.label3.Size = new System.Drawing.Size(134, 20);
             this.label3.TabIndex = 2;
-            this.label3.Text = "label3";
+            this.label3.Text = "Inventory Number";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(539, 190);
+            this.label4.Location = new System.Drawing.Point(56, 151);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(51, 20);
+            this.label4.Size = new System.Drawing.Size(52, 20);
             this.label4.TabIndex = 3;
-            this.label4.Text = "label4";
+            this.label4.Text = "Count";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(308, 251);
+            this.label5.Location = new System.Drawing.Point(56, 99);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(51, 20);
+            this.label5.Size = new System.Drawing.Size(98, 20);
             this.label5.TabIndex = 4;
-            this.label5.Text = "label5";
+            this.label5.Text = "object Name";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(452, 290);
+            this.label6.Location = new System.Drawing.Point(258, 151);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(51, 20);
+            this.label6.Size = new System.Drawing.Size(44, 20);
             this.label6.TabIndex = 5;
-            this.label6.Text = "label6";
+            this.label6.Text = "Price";
+            // 
+            // numberBox
+            // 
+            this.numberBox.Location = new System.Drawing.Point(60, 61);
+            this.numberBox.Name = "numberBox";
+            this.numberBox.Size = new System.Drawing.Size(127, 26);
+            this.numberBox.TabIndex = 6;
+            // 
+            // dateBox
+            // 
+            this.dateBox.Location = new System.Drawing.Point(262, 61);
+            this.dateBox.Name = "dateBox";
+            this.dateBox.Size = new System.Drawing.Size(180, 26);
+            this.dateBox.TabIndex = 7;
+            // 
+            // inventoryNumBox
+            // 
+            this.inventoryNumBox.Location = new System.Drawing.Point(483, 61);
+            this.inventoryNumBox.Name = "inventoryNumBox";
+            this.inventoryNumBox.Size = new System.Drawing.Size(227, 26);
+            this.inventoryNumBox.TabIndex = 8;
+            // 
+            // objectNameBox
+            // 
+            this.objectNameBox.Location = new System.Drawing.Point(60, 122);
+            this.objectNameBox.Name = "objectNameBox";
+            this.objectNameBox.Size = new System.Drawing.Size(539, 26);
+            this.objectNameBox.TabIndex = 9;
+            // 
+            // countBox
+            // 
+            this.countBox.Location = new System.Drawing.Point(60, 174);
+            this.countBox.Name = "countBox";
+            this.countBox.Size = new System.Drawing.Size(100, 26);
+            this.countBox.TabIndex = 10;
+            // 
+            // priceBox
+            // 
+            this.priceBox.Location = new System.Drawing.Point(262, 174);
+            this.priceBox.Name = "priceBox";
+            this.priceBox.Size = new System.Drawing.Size(100, 26);
+            this.priceBox.TabIndex = 11;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(60, 206);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(595, 134);
+            this.dataGridView1.TabIndex = 12;
+            // 
+            // addBtn
+            // 
+            this.addBtn.Location = new System.Drawing.Point(135, 356);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(150, 45);
+            this.addBtn.TabIndex = 13;
+            this.addBtn.Text = "ADD";
+            this.addBtn.UseVisualStyleBackColor = true;
+            // 
+            // cancelBtn
+            // 
+            this.cancelBtn.Location = new System.Drawing.Point(324, 356);
+            this.cancelBtn.Name = "cancelBtn";
+            this.cancelBtn.Size = new System.Drawing.Size(150, 45);
+            this.cancelBtn.TabIndex = 14;
+            this.cancelBtn.Text = "CANCEL";
+            this.cancelBtn.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(790, 438);
+            this.Controls.Add(this.cancelBtn);
+            this.Controls.Add(this.addBtn);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.priceBox);
+            this.Controls.Add(this.countBox);
+            this.Controls.Add(this.objectNameBox);
+            this.Controls.Add(this.inventoryNumBox);
+            this.Controls.Add(this.dateBox);
+            this.Controls.Add(this.numberBox);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -104,6 +194,7 @@
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -117,6 +208,15 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox numberBox;
+        private System.Windows.Forms.TextBox dateBox;
+        private System.Windows.Forms.TextBox inventoryNumBox;
+        private System.Windows.Forms.TextBox objectNameBox;
+        private System.Windows.Forms.TextBox countBox;
+        private System.Windows.Forms.TextBox priceBox;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button addBtn;
+        private System.Windows.Forms.Button cancelBtn;
     }
 }
 
--- lab4windowsform/lab4windowsform/Form1.cs
+++ lab4windowsform/lab4windowsform/Form1.cs
@@ -21,5 +21,10 @@ namespace lab4windowsform
         {
 
         }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
